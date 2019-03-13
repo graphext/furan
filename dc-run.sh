e950d643abb711cfc4d296a000d8f3f1faf41463
mikeykhalil <michael.khalil@dollarshaveclub.com>
2019-03-12 17:41:16 -0700

    more nonsense

--- dc-run.sh
+++ dc-run.sh
@@ -1,5 +1,5 @@
 #!/bin/dumb-init /bin/sh
-
+## some other nonsense comment
 echo "starting (delay)"
 sleep 20
 exec /go/bin/furan -k -i -n scylla -f kafka:9092 server --log-to-sumo=false --consul-addr consul:8500
