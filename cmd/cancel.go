2d609703e697db873e6098a950cd9ef6e15e3cc5
Benjamen Keroack <benjamen@dollarshaveclub.com>
2020-10-05 15:28:59 -0700

    cmd: implement cancel command

--- cmd/cancel.go
+++ cmd/cancel.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/gofrs/uuid"
 	"github.com/spf13/cobra"
 
-	"github.com/dollarshaveclub/furan/pkg/generated/furanrpc"
+	"github.com/dollarshaveclub/furan/pkg/client"
 )
 
 // triggerCmd represents the trigger command
@@ -11,16 +15,36 @@ var cancelCmd = &cobra.Command{
 	Use:   "cancel",
 	Short: "Cancel a running build",
 	Long:  `Cancel a running build in a remote Furan cluster`,
-	Run:   cancel,
+	RunE:  cancel,
 }
 
-var cancelReq = &furanrpc.BuildCancelRequest{}
+var cancelBuildID string
 
 func init() {
-	cancelCmd.PersistentFlags().StringVar(&clientops.Address, "remote-host", "", "Remote Furan server with gRPC port (eg: furan.me.com:4001)")
-	cancelCmd.PersistentFlags().StringVar(&cancelReq.BuildId, "build-id", "", "Build ID")
+	setclientflags(cancelCmd)
+	cancelCmd.PersistentFlags().StringVar(&cancelBuildID, "build-id", "", "Build ID")
 	RootCmd.AddCommand(cancelCmd)
 }
 
-func cancel(cmd *cobra.Command, args []string) {
+func cancel(cmd *cobra.Command, args []string) error {
+	rb, err := client.New(clientops)
+	if err != nil {
+		return fmt.Errorf("error creating rpc client: %w", err)
+	}
+	defer rb.Close()
+
+	id, err := uuid.FromString(cancelBuildID)
+	if err != nil {
+		return fmt.Errorf("malformed or invalid build id: %w", err)
+	}
+
+	ctx := context.Background()
+
+	err = rb.CancelBuild(ctx, id)
+	if err != nil {
+		return fmt.Errorf("error sending cancel request: %w", err)
+	}
+
+	fmt.Println("build cancellation requested: " + id.String())
+	return nil
 }
